4ce7e858f9fa4d64eafb43b8ffa295c8fcc7fccf
Alex Wang <alexw@nicira.com>
2014-03-19 10:01:25 -0700

    libbfd+monitor: Remove the include of <config.h>.
    The '#include <config.h>' is only required by the Open Vswitch.
    So this commit removes it.
    
    Signed-off-by: Alex Wang <alexw@nicira.com>

--- monitor.c
+++ monitor.c
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
-#include <config.h>
-
 #include "monitor.h"
 #include "monitor-aux.h"
 #include "monitor-heap.h"
